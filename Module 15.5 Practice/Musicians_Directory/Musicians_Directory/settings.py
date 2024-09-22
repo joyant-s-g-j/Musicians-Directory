69ab1edfc9d84781b766c88a0ea4d3364aa4bef8
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-09-22 17:20:16 +0600

    setup complete

--- Module 15.5 Practice/Musicians_Directory/Musicians_Directory/settings.py
+++ Module 15.5 Practice/Musicians_Directory/Musicians_Directory/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'musician',
+    'album',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Musicians_Directory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
